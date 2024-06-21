ad29f9e1052e94aa91e62121d1e56afbce904c16
Kyle Upton <kyleaupton@gmail.com>
2024-06-21 18:24:09 -0400

    added linter

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    node: true,
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    '@vue/typescript/recommended',
+    'eslint:recommended',
+    'prettier',
+  ],
+  plugins: ['prettier'],
+  rules: {
+    // Required
+    'prettier/prettier': 'error',
+  },
+};
