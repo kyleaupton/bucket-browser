dbcead009afaaa37a46b8a85c97f5adf11645cb4
Kyle Upton <kyleaupton@gmail.com>
2024-06-25 22:53:30 -0400

    added prime vue

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
 import App from './App.vue';
 
 import './style.css';
 
 createApp(App)
   .use(createPinia())
+  .use(PrimeVue, {
+    theme: {
+      preset: Aura,
+    },
+  })
   .mount('#app')
   .$nextTick(() => {
     postMessage({ payload: 'removeLoading' }, '*');
