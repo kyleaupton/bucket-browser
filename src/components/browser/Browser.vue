b90427bba661a288fefbf9c878cf7b02ab9816b9
Kyle Upton <kyleaupton@gmail.com>
2024-06-29 14:51:54 -0400

    spinner logic

--- src/components/browser/Browser.vue
+++ src/components/browser/Browser.vue
@@ -5,17 +5,24 @@
   >
     <div>No Connection Selected</div>
   </div>
-  <div
-    v-else-if="!items.length"
-    class="h-full select-none flex justify-center items-center"
-  >
-    <div>No Items</div>
-  </div>
 
   <div v-else class="flex flex-col h-full">
     <BrowserNavigation />
     <div class="flex-grow overflow-hidden mb-2">
-      <VirtualScroller class="h-full" :items="items" :item-size="48">
+      <div v-if="loading" class="h-full flex justify-center items-center">
+        <ProgressSpinner
+          style="width: 50px; height: 50px"
+          stroke-width="6"
+          animation-duration=".5s"
+        />
+      </div>
+      <div
+        v-else-if="!items.length && !fetching"
+        class="h-full select-none flex justify-center items-center"
+      >
+        <div>No Items</div>
+      </div>
+      <VirtualScroller v-else class="h-full" :items="items" :item-size="48">
         <template #item="{ item }">
           <BrowserItem :item="item" />
         </template>
@@ -28,6 +35,7 @@
 import { ref, watch, computed } from 'vue';
 import { storeToRefs } from 'pinia';
 import VirtualScroller from 'primevue/virtualscroller';
+import ProgressSpinner from 'primevue/progressspinner';
 import { Bucket, _Object, CommonPrefix } from '@aws-sdk/client-s3';
 import {
   listBucketsChannel,
@@ -40,6 +48,8 @@ import BrowserItem from './BrowserItem.vue';
 const layoutStore = useLayoutStore();
 const { path, selectedConnection } = storeToRefs(layoutStore);
 
+const loading = ref(false);
+const fetching = ref(false);
 const items = ref<(Bucket | _Object | CommonPrefix)[]>([]);
 const selectedBucket = computed(() => path.value.split('/')[1]);
 const selectedObject = computed(() => path.value.split('/').slice(2).join('/'));
@@ -49,6 +59,14 @@ watch(path, () => fetchItems());
 
 const fetchItems = async () => {
   if (selectedConnection.value) {
+    items.value = [];
+    fetching.value = true;
+
+    // Only display spinner if fetching takes longer than 1 second
+    const timeout = setTimeout(() => {
+      loading.value = true;
+    }, 500);
+
     if (!selectedBucket.value) {
       const { Buckets } = await window.ipcInvoke(
         listBucketsChannel,
@@ -74,8 +92,21 @@ const fetchItems = async () => {
 
       items.value = [..._commonPrefixes, ..._contents];
     }
+
+    loading.value = false;
+    clearTimeout(timeout);
   }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.1s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
---
+++ src/components/browser/BrowserSkeleton.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div v-for="index in 20" :key="index" class="mx-4 p-2 flex gap-2">
+      <Skeleton width="36px" height="32px" class="h-2 w-2" />
+      <Skeleton height="32px" class="h-2 w-2" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Skeleton from 'primevue/skeleton';
+</script>
+
+<style scoped></style>
