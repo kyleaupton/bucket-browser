a0a457266053039c1cb29ee84b1f7dd5f9b04b60
Kyle Upton <kyleaupton@gmail.com>
2024-08-31 00:49:02 -0400

    remove unused deps and move to dev

--- drizzle.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'drizzle-kit';
-
-export default defineConfig({
-  schema: './electron/main/db/schema.ts',
-  out: './electron/main/db/drizzle',
-  dialect: 'sqlite',
-});
