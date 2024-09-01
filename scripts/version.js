009bab82ca31de0ff1f99ff7ff95702736bd9806
Kyle Upton <kyleaupton@gmail.com>
2024-09-01 13:47:33 -0400

    version test

--- scripts/version.js
+++ scripts/version.js
@@ -1,18 +1,21 @@
+/**
+ * This script is used to get the version of the package.json file.
+ * It is used in the build process, and it will append the version to the
+ * GitHub Actions env file.
+ */
+
 import fs from 'node:fs';
 import path from 'node:path';
 import url from 'node:url';
 
-// For some reason, on mac we need to use console.log
-// and on windows we need to use process.stdout.write
+if (!process.env.GITHUB_ENV) {
+  throw new Error('GITHUB_ENV not found');
+}
 
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 const packageJson = path.join(__dirname, '..', 'package.json');
 
 const { version } = JSON.parse(fs.readFileSync(packageJson, 'utf-8'));
-const string = `APP_VERSION=${version}`;
+const envVar = `APP_VERSION=${version}`;
 
-if (process.platform === 'win32') {
-  process.stdout.write(string);
-} else {
-  console.log(string);
-}
+fs.appendFileSync(process.env.GITHUB_ENV, envVar);
