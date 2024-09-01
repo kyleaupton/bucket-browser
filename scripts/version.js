a412e5a110e07265a0ffbd9f904a6acf4e8c00da
Kyle Upton <kyleaupton@gmail.com>
2024-09-01 02:30:42 -0400

    Update version.js

--- scripts/version.js
+++ scripts/version.js
@@ -1,6 +1,8 @@
 import fs from 'node:fs';
 import path from 'node:path';
 
-console.log(
-  JSON.parse(fs.readFileSync(path.resolve('package.json'), 'utf-8')).version,
-);
+// For some reason, on mac we need to use console.log
+// and on windows we need to use process.stdout.write
+
+const fun = process.platform === 'win32' ? process.stdout.write : console.log;
+fun(JSON.parse(fs.readFileSync(path.resolve('package.json'), 'utf-8')).version);
