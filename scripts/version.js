2b1e8d3a3f0ca3fc6c1ea4ef2046612aafc171db
Kyle Upton <kyleaupton@gmail.com>
2024-09-01 02:35:19 -0400

    version fix

--- scripts/version.js
+++ scripts/version.js
@@ -4,10 +4,12 @@ import path from 'node:path';
 // For some reason, on mac we need to use console.log
 // and on windows we need to use process.stdout.write
 
-const { version } = JSON.parse(fs.readFileSync(path.resolve('package.json'), 'utf-8'))
+const { version } = JSON.parse(
+  fs.readFileSync(path.resolve('package.json'), 'utf-8'),
+);
 
 if (process.platform === 'win32') {
-  process.stdout.write(version)
+  process.stdout.write(version);
 } else {
-  console.log(version)
+  console.log(version);
 }
