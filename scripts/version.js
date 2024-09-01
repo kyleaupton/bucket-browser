970c5a9032ec24e9fc698282ad9f57f6be36c4cb
Kyle Upton <kyleaupton@gmail.com>
2024-09-01 13:17:59 -0400

    Update version.js

--- scripts/version.js
+++ scripts/version.js
@@ -1,14 +1,15 @@
 import fs from 'node:fs';
 import path from 'node:path';
+import url from 'node:url';
 
 // For some reason, on mac we need to use console.log
 // and on windows we need to use process.stdout.write
 
-const { version } = JSON.parse(
-  fs.readFileSync(path.resolve('package.json'), 'utf-8'),
-);
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const packageJson = path.join(__dirname, '..', 'package.json');
 
-const string = `APP_VERSION=${version}`
+const { version } = JSON.parse(fs.readFileSync(packageJson, 'utf-8'));
+const string = `APP_VERSION=${version}`;
 
 if (process.platform === 'win32') {
   process.stdout.write(string);
